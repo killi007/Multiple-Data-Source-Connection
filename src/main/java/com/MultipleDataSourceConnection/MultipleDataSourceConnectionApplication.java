9f9613ccca944388e80f121612dda0473a1119ed
TS1019 <TS1019@TS2021>
2024-08-04 09:14:54 +0530

    initial commit

---
+++ src/main/java/com/MultipleDataSourceConnection/MultipleDataSourceConnectionApplication.java
@@ -0,0 +1,13 @@
+package com.MultipleDataSourceConnection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleDataSourceConnectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultipleDataSourceConnectionApplication.class, args);
+	}
+
+}
